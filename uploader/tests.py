75fcc5c8ce52ca1d5bd3ad008eb16b6a7859d526
Andrew <ghobrial.andrew@gmail.com>
2019-04-23 17:04:30 -0400

    created test to analyze result of user uploading image with title

--- uploader/tests.py
+++ uploader/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from uploader.models import Post
 
-# Create your tests here.
+class PostTestCase(TestCase):
+
+    def test_post_title(self):
+        """The correct title is returned after a user uploads an image"""
+        testImage = Post.objects.create(title="test image!")
+        self.assertEqual(testImage.__str__(), 'test image!')
